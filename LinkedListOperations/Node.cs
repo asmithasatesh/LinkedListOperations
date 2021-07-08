f332003f9e7def0964148fe0b5083ac1d4f99f9b
asmithasatesh <asmithasatesh@gmail.com>
2021-07-08 20:43:12 +0530

    [Asmitha]Insertion At Last

---
+++ LinkedListOperations/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListOperations
+{
+    public class Node
+    {
+        //Class Node with data and next pointer
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
---
+++ LinkedListOperations/OperationsOnLinkedList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListOperations
+{
+    class OperationsOnLinkedList
+    {
+        public Node head;
+        //Insert data to Last
+        public void InsertLast(int data)
+        {
+            Node newNode = new Node(data);
+            if(head==null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                Node lastNode = GetLastNode();
+                lastNode.next = newNode;
+            }
+        }
+        //To get Last Node
+        public Node GetLastNode()
+        {
+            Node temp = this.head;
+            while(temp.next!=null)
+            {
+                temp = temp.next;
+            }
+            return temp;
+        }
+        //Display Linked list Data
+        public void Display()
+        {
+            int count = 1;
+            Node temp = this.head;
+            while (temp != null)
+            {
+                Console.WriteLine("Element {0} of Linked List: {1} ",count,temp.data);
+                temp = temp.next;
+                count++;
+            }
+        }
+    }
+}
